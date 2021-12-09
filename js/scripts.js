a239b2ab5d6d4302a9dcf8b5290031fb84b48e3e
Nathan Conn <nconn34@gmail.com>
2021-12-08 21:55:59 -0800

    Created gh-page

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,26 @@
-// Business logic for dice rolls
-
+//
 
 console.log("hello!");
 console.log("pig dice rules!");
 console.log(Math.ceil(Math.random()*6));
-
 const roll1 = Math.ceil(Math.random()*6);
 console.log("roll 1 :", roll1);
-
 const roll2 = Math.ceil(Math.random()*6);
 console.log("roll 2 :", roll2);
-
 let player1 = roll1 + roll2
 console.log("Player 1 score:", player1)
-
 // function rollDice(){
 //    return Math.ceil(Math.random()*6);
 // }
-
 // Player.prototype.endTurn = function () {
 //     this.totalScore += this.currentScore;
 //     this.currentScore = 0;
 //   }
-
   function rollDice(){
     return Math.ceil(Math.random()*6);
  }
+
+ // Math.round(Math.random()*5)+1
 
 // function roll(){
 //   const roll = rollDice();
@@ -38,25 +33,17 @@ console.log("Player 1 score:", player1)
 //   }
 //   return score
 // };
-
-
-
-
 // Objects
-
 function Player(name, turn, totalScore){
     this.name=name;
     this.turn=turn;
     this.totalScore=totalScore;
 };
-
 // Prototype section
-
 // Player.prototype.endTurn = function () {
 //     this.totalScore += this.currentScore;
 //     this.currentScore = 0;
 // }
-
 // function addDice(number, player){
 //    if (number===1){
 //        player.totalScore=0;
@@ -65,7 +52,7 @@ function Player(name, turn, totalScore){
 //    }
 // }
 
-  Player.protoype.addScore = function(number){
+  Player.prototype.addScore = function(number){
     this.totalScore = this.totalScore + number
 }
 
@@ -86,7 +73,6 @@ function Player(name, turn, totalScore){
          return player2
      }
   }
-
   function switchPlayer(player1, player2){
       if (player1.turn === true){
           player1.turn = false;
@@ -96,9 +82,9 @@ function Player(name, turn, totalScore){
           player2.turn = false;
       }
   }
-
   $(document).ready(function(){
       let dice = 0;
+      let total = 0;
       let player1 = new Player("player1", true, 0);
       let player2 = new Player("player2", false, 0);
       let activePlayer = playerTurn(player1, player2);
@@ -106,27 +92,31 @@ function Player(name, turn, totalScore){
       $("#current-player-name").html("The current player is: " + activePlayer.name);
       $("#dice").click(function(){
           dice = rollDice();
+          total = dice+
           $("#roll").html("You rolled a: " + dice);
-          if(dice===1){
+          if(dice === 1){
               switchPlayer(player1, player2);
               if(activePlayer.name === "player1"){
                   player1.totalScore = 0;
-                  $("#player1-score").append("Round " + roundCounter + " score: " + activePlayer.totalScore + "<br>");
+                  $("#player1-score").append("Round: " + roundCounter + " score: " + activePlayer.totalScore + "<br>");
                   $("#roll").html("");
                   $("#current-score").html("");
+                  
               } else if(activePlayer.name === "player2"){
                   player2.totalScore = 0;
                   $("#player2-score").append("Round " + roundCounter + " score: " + activePlayer.totalScore + "<br>");
                   $("#roll").html("");
                   $("#current-score").html("");
                   roundCounter++;
-              }                  
-              currentPlayer = playerTurn(player1, player2);
+                  total
+              }               
+              activePlayer = playerTurn(player1, player2);
               $("#current-player-name").html("The current player is: " + activePlayer.name);
-              }
+            } 
               addRoll(dice, activePlayer);
               $("#current-score").html("Your current score is: " + activePlayer.totalScore)
-          })
+              
+          });
           $("#finish").click(function(){
               if(activePlayer.name === "player1"){
                   $("#player1-score").append("Round " + roundCounter + " score: " +activePlayer.totalScore + "<br>");
@@ -145,9 +135,7 @@ function Player(name, turn, totalScore){
               $("#current-player-name").html("The current player is: " + activePlayer.name);
           })
       })
-  
+    
 
 
-
-
-
+// Previous code you were working with is in the TEST.md page.
